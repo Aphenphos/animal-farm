2714fedcc27d1e9da167c786099640ffcdcb7947
Aphenphos <MaxwillWinters@gmail.com>
2022-09-12 18:11:01 -0400

    I think this is hot it is supposed to look?

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
+import backgroundImg from '../../background.png';
 import './Main.css';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {animals.map((item) => (
+        <Animal key={item.name} {...item} />
+      ))}
+    </main>
+  );
 }
