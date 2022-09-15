c4a9b18ca75079f80a92f098951873ae6be1c810
Aphenphos <MaxwillWinters@gmail.com>
2022-09-15 17:39:27 -0400

    added sounds

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Animal.css';
 
+
 export default function Animal(props) {
+  let audio = new Audio();
+  const start = () => {
+    console.log(props.type);
+    audio = new Audio(`${process.env.PUBLIC_URL}/animalSounds/${props.type}.mp3`);
+    console.log(audio);
+    audio.play();
+  };
+
+
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img className="img" alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
-      <span className="name">{props.name}</span>
-      <span>{props.says}</span>
+      <button onClick={start}>
+        <img className="img" alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+        <span className="name">{props.name}</span>
+        <span>{props.says}</span>
+      </button>
     </div>
   );
 }
